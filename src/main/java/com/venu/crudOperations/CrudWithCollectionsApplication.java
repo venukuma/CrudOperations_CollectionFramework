cf40e74e9867cc2311ae5f40695c5689db7df4a9
venukuma <maddelavenukumar@gmail.com>
2019-10-17 07:54:30 +0530

    Skelton Project

---
+++ src/main/java/com/venu/crudOperations/CrudWithCollectionsApplication.java
@@ -0,0 +1,13 @@
+package com.venu.crudOperations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudWithCollectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudWithCollectionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/venu/crudOperations/CrudWithCollectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venu.crudOperations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudWithCollectionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
